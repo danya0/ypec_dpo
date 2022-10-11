7fad34e75f3dce2558622ea6da4e16f1f171c48d
Samarkin danya0 Danil <samarkin.danil@yandex.ru>
2022-10-11 11:08:15 +0300

    Картинки перенес в сам проект

--- src/script/index.ts
+++ src/script/index.ts
@@ -2,3 +2,9 @@
 import '../scss/index.scss'
 // dpo scripts
 import './dpo/index'
+
+import '../img/it.jpg'
+import '../img/him.jpg'
+import '../img/3.jpg'
+import '../img/4.jpg'
+import '../img/5.jpg'
